1c66f050b45b9231ed3e2456e9f69be5201836e7
RAbh-inav <abhinavramesh270402@gmail.com>
2022-07-25 15:50:22 +0000

    test_seq_detect_1011.py

--- level1_design2/test_seq_detect_1011.py
+++ level1_design2/test_seq_detect_1011.py
@@ -14,7 +14,7 @@ from cocotb.triggers import RisingEdge, FallingEdge
 async def test_seq_bug1(dut):
     """Test for seq detection """
 
-    clock = Clock(dut.clk, 10, units="us")  # Create a 10us period clock on port clk
+    clock = Clock(dut.clk, 10, units="ns")  # Create a 10us period clock on port clk
     cocotb.start_soon(clock.start())        # Start the clock
     # reset
     dut.reset.value = 1
@@ -24,7 +24,7 @@ async def test_seq_bug1(dut):
 async def test_seq_bug2(dut):
     """Test for seq detection """
 
-    clock = Clock(dut.clk, 10, units="us")  # Create a 10us period clock on port clk
+    clock = Clock(dut.clk, 10, units="ns")  # Create a 10us period clock on port clk
     cocotb.start_soon(clock.start())        # Start the clock
 
     # reset
@@ -34,5 +34,11 @@ async def test_seq_bug2(dut):
     await FallingEdge(dut.clk)
     dut.inp_bit.value=1
     await FallingEdge(dut.clk) 
-    assert dut.next_state.value == 1, f"sequence detector result is incorrect: {dut.current_state.value} != IDLE"
+    dut.inp_bit.value=0
+    await FallingEdge(dut.clk) 
+    dut.inp_bit.value=1
+    await FallingEdge(dut.clk)
+    dut.inp_bit.value=1
+    await FallingEdge(dut.clk) 
+    assert dut.seq_seen.value == 1, f"sequence detector result is incorrect: {dut.current_state.value} != 1011"
 
