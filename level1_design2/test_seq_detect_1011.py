1021016cd7f803c462ba4ede14f6f5f848733b66
RAbh-inav <abhinavramesh270402@gmail.com>
2022-07-25 17:21:37 +0000

    test_seq_detect_1011.py

--- level1_design2/test_seq_detect_1011.py
+++ level1_design2/test_seq_detect_1011.py
@@ -9,17 +9,6 @@ from pathlib import Path
 import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import RisingEdge, FallingEdge,ClockCycles
-
-@cocotb.test()
-async def test_seq_bug1(dut):
-    """Test for seq detection """
-
-    clock = Clock(dut.clk, 10, units="ns")  # Create a 10us period clock on port clk
-    cocotb.start_soon(clock.start())        # Start the clock
-    # reset
-    dut.reset.value = 1
-    await FallingEdge(dut.clk) 
-    assert dut.current_state.value == 0, f"sequence detector result is incorrect: {dut.current_state.value} != IDLE"
 @cocotb.test()
 async def test_seq_bug2(dut):
     """Test for seq detection """
