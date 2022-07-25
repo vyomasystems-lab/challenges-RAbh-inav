40459278b5f2bfaa130a3550ee455f9f9e70a746
RAbh-inav <abhinavramesh270402@gmail.com>
2022-07-25 20:12:29 +0000

    shift_and_add_multiplier_tb.py

---
+++ level3_design/shift_and_add_binary_multiplier_buggy/shift_and_add_binary_multiplier_tb.py
@@ -0,0 +1,24 @@
+# See LICENSE.vyoma for details
+
+# SPDX-License-Identifier: CC0-1.0
+
+import cocotb
+from cocotb.clock import Clock
+from cocotb.triggers import RisingEdge, FallingEdge,ClockCycles
+
+@cocotb.test()
+async def shift_and_add_multiplier_bug1(dut):
+    """Test for bug in shift and add multiplier """
+    clock = Clock(dut.clk, 10, units="ns")  # Create a 10ns period clock on port clk
+    cocotb.start_soon(clock.start())        # Start the clock
+    # reset
+    dut.reset.value = 1
+    await FallingEdge(dut.clk)
+    dut.reset.value = 0
+    await FallingEdge(dut.clk)
+    dut.A.value=1
+    dut.B.value=1
+    await FallingEdge(dut.clk)
+    assert dut.C.value == 1, f"sequence detector result is incorrect: {dut.C.value} != 1"
+
+
