989b6d389b5fb9becfecc508d62e16abafa2903b
RAbh-inav <abhinavramesh270402@gmail.com>
2022-07-25 14:18:16 +0000

    test_mux.py

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -14,3 +14,13 @@ async def test_mux2(dut):
     dut.inp12.value=inp12
     await Timer(4, units='ns')
     assert dut.out.value == 2, f"mux result is incorrect: {dut.out.value} != inp13"
+
+@cocotb.test()
+async def test_mux2(dut):
+    """Test for mux2"""
+    select =10
+    inp10=2
+    dut.sel.value=select
+    dut.inp10.value=inp10
+    await Timer(4, units='ns')
+    assert dut.out.value == 2, f"mux result is incorrect: {dut.out.value} != inp10"
