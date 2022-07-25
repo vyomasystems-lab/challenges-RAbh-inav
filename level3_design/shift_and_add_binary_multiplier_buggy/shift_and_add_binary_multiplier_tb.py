d3254d5e708c8cb6c15d5a7469e40da66349242d
RAbh-inav <abhinavramesh270402@gmail.com>
2022-07-25 20:25:25 +0000

    shift_and_add_binary_multiplier_buggy.v

--- level3_design/shift_and_add_binary_multiplier_buggy/shift_and_add_binary_multiplier_tb.py
+++ level3_design/shift_and_add_binary_multiplier_buggy/shift_and_add_binary_multiplier_tb.py
@@ -12,9 +12,9 @@ async def shift_and_add_multiplier_bug1(dut):
     clock = Clock(dut.clk, 10, units="ns")  # Create a 10ns period clock on port clk
     cocotb.start_soon(clock.start())        # Start the clock
     # reset
-    dut.reset.value = 1
+    dut.rst.value = 1
     await FallingEdge(dut.clk)
-    dut.reset.value = 0
+    dut.rst.value = 0
     await FallingEdge(dut.clk)
     dut.A.value=1
     dut.B.value=1
