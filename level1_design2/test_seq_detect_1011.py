1de40ff11c1b2450b4231f48cfb9ee437157e4a7
RAbh-inav <abhinavramesh270402@gmail.com>
2022-07-25 17:41:46 +0000

    test_seq_detect_1011.py

--- level1_design2/test_seq_detect_1011.py
+++ level1_design2/test_seq_detect_1011.py
@@ -41,3 +41,35 @@ async def test_seq_bug2(dut):
     await FallingEdge(dut.clk)
     assert dut.current_state.value == 4, f"sequence detector result is incorrect: {dut.next_state.value} != 1011"
 
+
+@cocotb.test()
+async def test_seq_bug2(dut):
+    """Test for seq detection """
+
+    clock = Clock(dut.clk, 10, units="ns")  # Create a 10us period clock on port clk
+    cocotb.start_soon(clock.start())        # Start the clock
+
+    # reset
+    dut.reset.value = 1
+    dut.inp_bit.value=0
+    await FallingEdge(dut.clk)
+    dut.reset.value = 0
+    await FallingEdge(dut.clk)
+    await FallingEdge(dut.clk)
+
+    dut.inp_bit.value=1
+    cocotb.log.info(dut.current_state.value)
+    await FallingEdge(dut.clk)
+    dut.inp_bit.value=1
+    cocotb.log.info(dut.current_state.value)
+    await FallingEdge(dut.clk)
+    dut.inp_bit.value=0
+    cocotb.log.info(dut.current_state.value)
+    await FallingEdge(dut.clk)    
+    dut.inp_bit.value=1
+    cocotb.log.info(dut.current_state.value)
+    await FallingEdge(dut.clk)
+    dut.inp_bit.value=1
+    cocotb.log.info(dut.current_state.value)
+    await FallingEdge(dut.clk)
+    assert dut.current_state.value == 4, f"sequence detector result is incorrect: {dut.next_state.value} != 1011"
