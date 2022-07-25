38e5f78ac6135f06c923a0d6aa2753b9f24417bc
RAbh-inav <abhinavramesh270402@gmail.com>
2022-07-25 21:13:42 +0000

    shift_and_add_binary_multiplier_buggy.v

--- level3_design/shift_and_add_binary_multiplier_buggy/shift_and_add_binary_multiplier_buggy.v
+++ level3_design/shift_and_add_binary_multiplier_buggy/shift_and_add_binary_multiplier_buggy.v
@@ -5,8 +5,8 @@ input clk,rst;
 input [m-1:0] A;
 input [n-1:0] B;
 output reg [m+n-1:0]C;
-reg [m+n-1:0]A1;
-reg [m+n-1:0]B1;
+reg [m+n-2:0]A1;
+reg [n-1:0]B1;
 always@(posedge clk or posedge rst )
 begin
 		if (rst)
@@ -17,7 +17,7 @@ begin
 		begin
 		C=0;
 		A1[m-1:0]=A;
-		A1[m+n-1:m]=0;
+		A1[m+n-2:m]=0;
 		B1=B;
         for (i=0;i<n;i=i+1)
         begin
@@ -27,7 +27,7 @@ begin
 			end
 			else if (B1[i]==1'b1)
 			begin
-				C=C+(A1>>1);
+				C=C+(A1>>i);
 			end
 		end
 		end
