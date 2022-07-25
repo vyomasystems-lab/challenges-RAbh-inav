cc5aa677b235e7e13a3d3f1e97e7d048cb442e3c
RAbh-inav <abhinavramesh270402@gmail.com>
2022-07-25 16:34:22 +0000

    test_seq_detect_1011.py

--- level1_design2/test_seq_detect_1011.py
+++ level1_design2/test_seq_detect_1011.py
@@ -32,14 +32,5 @@ async def test_seq_bug2(dut):
     dut.inp_bit.value=1
     cocotb.log.info(dut.current_state.value)
     await FallingEdge(dut.clk)
-    dut.inp_bit.value=0
-    cocotb.log.info(dut.current_state.value)
-    await FallingEdge(dut.clk)
-    dut.inp_bit.value=1
-    cocotb.log.info(dut.current_state.value)
-    await FallingEdge(dut.clk)
-    dut.inp_bit.value=1
-    cocotb.log.info(dut.current_state.value)
-    await FallingEdge(dut.clk)
     assert dut.seq_seen.value == 1, f"sequence detector result is incorrect: {dut.next_state.value} != 1011"
 
