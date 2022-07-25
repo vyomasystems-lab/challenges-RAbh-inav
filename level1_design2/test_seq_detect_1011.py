3905b46db8ddbff8277d4916807f9dcd31f565b7
RAbh-inav <abhinavramesh270402@gmail.com>
2022-07-25 17:52:40 +0000

    test_seq_detect_1011.py

--- level1_design2/test_seq_detect_1011.py
+++ level1_design2/test_seq_detect_1011.py
@@ -55,7 +55,6 @@ async def test_seq_bug2(dut):
     await FallingEdge(dut.clk)
     dut.reset.value = 0
     await FallingEdge(dut.clk)
-    await FallingEdge(dut.clk)
     dut.inp_bit.value=1
     cocotb.log.info(dut.current_state.value)
     await FallingEdge(dut.clk)
