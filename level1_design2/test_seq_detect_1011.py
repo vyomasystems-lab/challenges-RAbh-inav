72f6d642db45ee3daf271e114c6aa69aaf649f07
RAbh-inav <abhinavramesh270402@gmail.com>
2022-07-25 17:42:32 +0000

    test_seq_detect_1011.py

--- level1_design2/test_seq_detect_1011.py
+++ level1_design2/test_seq_detect_1011.py
@@ -14,7 +14,7 @@ from cocotb.triggers import RisingEdge, FallingEdge,ClockCycles
 async def test_seq_bug2(dut):
     """Test for seq detection """
 
-    clock = Clock(dut.clk, 10, units="ns")  # Create a 10us period clock on port clk
+    clock = Clock(dut.clk, 10, units="us")  # Create a 10us period clock on port clk
     cocotb.start_soon(clock.start())        # Start the clock
 
     # reset
