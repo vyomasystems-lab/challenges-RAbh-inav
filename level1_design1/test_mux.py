841dd03682b07a3d7203e3e5fc49dfd099dbc5e1
RAbh-inav <abhinavramesh270402@gmail.com>
2022-07-25 13:50:05 +0000

    test_mux.py

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -6,7 +6,7 @@ from cocotb.triggers import Timer
 @cocotb.test()
 async def test_mux(dut):
     """Test for mux2"""
-    select =13
+    select =10
     dut.sel.value=select
     await Timer(2, units='ns')
-    assert dut.out.value == inp12, f"mux result is incorrect: {dut.out.value} != inp12"
+    assert dut.out.value == inp10, f"mux result is incorrect: {dut.out.value} != inp10"
