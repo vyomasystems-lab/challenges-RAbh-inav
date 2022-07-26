fec9e7225aea12832683bb996af6122442ae232f
RAbh-inav <abhinavramesh270402@gmail.com>
2022-07-26 21:32:48 +0000

    test_seq_detect_1011.py

--- level1_design2/test_seq_detect_1011.py
+++ level1_design2/test_seq_detect_1011.py
@@ -2,13 +2,9 @@
 
 # SPDX-License-Identifier: CC0-1.0
 
-import os
-import random
-from pathlib import Path
-
 import cocotb
 from cocotb.clock import Clock
-from cocotb.triggers import RisingEdge, FallingEdge,ClockCycles
+from cocotb.triggers import FallingEdge
 
 @cocotb.test()
 async def test_seq_bug1(dut):
